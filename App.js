114f1dd062f6497a054eb4a9c25fe32d01c8acb2
awandev <awal.kurniawan@uin-alauddin.ac.id>
2020-12-24 22:02:48 +0800

    post props

--- App.js
+++ App.js
@@ -12,14 +12,24 @@ import {
   StatusBar,
 } from 'react-native';
 import HomeScreen from './src/screens/Home'
+import SearchResult from './src/screens/SearchResults'
 import Post from './src/components/Post'
+
+import feed from './assets/data/feed'
+
+const post1 = feed[0]
+
+
+
+
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         {/* <HomeScreen /> */}
-        <Post />
+        {/* <Post post={post1} /> */}
+        <SearchResult />
       </SafeAreaView>
     </>
   );
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,30 +3,32 @@ import { View, Text, Image } from 'react-native'
 import styles from './styles'
 
 
-const Post = () => {
+const Post = (props) => {
+    // console.log(props);
+    const post = props.post;
     return (
         <View style={styles.container}>
             <Image
-                source={{ uri: 'https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/6/2017/11/04133712/waterfall-750x500.jpg' }}
+                source={{ uri: post.image }}
                 style={styles.image}
             />
 
             {/* bed & bedroom */}
-            <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+            <Text style={styles.bedrooms}>{post.bed} bed {post.bedroom} bedroom</Text>
 
             {/* Type & Description */}
             <Text style={styles.description} numberOfLines={2}>
-                Now consider: What was this place like in your parents’ time? Your grandparents’? In many cases, the parks, beaches and campgrounds of today are surrounded by more development, or are themselves more developed, than they were decades ago.
+                {post.type}. {post.title}
             </Text>
 
             {/* old price and new price */}
             <Text style={styles.prices}>
-                <Text style={styles.oldPrice}>$36</Text>
-                <Text style={styles.price}>  $30 </Text>/ night
+                <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+                <Text style={styles.price}>  ${post.newPrice} </Text>/ night
             </Text>
 
             {/* total price */}
-            <Text style={styles.totalPrice}>$230 total</Text>
+            <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
 
         </View>
     )
